7cffc334076c5b5fea4e1b517e21fe0f8a87313e
ayushkr12 <ayushkr91997@gmail.com>
2025-02-05 03:37:33 +0530

    Update subtake.sh

--- subtake.sh
+++ subtake.sh
@@ -1,28 +1,35 @@
+#!/bin/bash
+
 # Usage: subtake <subs.txt> <threads> <output_file> [optional argument: output_file]
 
-# exit if custom nuclei templates path is not in environment variables or bash.rc
-if [[ $(echo $CUSTOM_NUCLEI_TEMPLATES_PATH) == "" ]]; then
-  echo "[error] unable to find custom nuclei template PATH"
+# Exit if custom nuclei templates path is not in environment variables or bashrc
+if [[ -z "$CUSTOM_NUCLEI_TEMPLATES_PATH" ]]; then
+  echo "[error] Unable to find custom nuclei template PATH"
   exit 1
 fi
 
-CUSTOM_SUBDOMAIN_TAKEOVER_TEMPLATES_PATH="$CUSTOM_NUCLEI_TEMPLATES_PATH"/subdomain_takeover_templates/
+CUSTOM_SUBDOMAIN_TAKEOVER_TEMPLATES_PATH="$CUSTOM_NUCLEI_TEMPLATES_PATH/subdomain_takeover_templates/"
 
 subtake() {
-  local subs_file=$1
-  local threads=$2
+  local subs_file="$1"
+  local threads="$2"
   local output_file="$3"
 
-  if [[ -n $output_file ]]; then
-    nuclei -t "http/takeovers" -t "$CUSTOM_SUBDOMAIN_TAKEOVER_TEMPLATES_PATH" -l "$subs_file" -c "$threads" 
-  else
+  if [[ -n "$output_file" ]]; then
     nuclei -t "http/takeovers" -t "$CUSTOM_SUBDOMAIN_TAKEOVER_TEMPLATES_PATH" -l "$subs_file" -c "$threads" -o "$output_file"
+  else
+    nuclei -t "http/takeovers" -t "$CUSTOM_SUBDOMAIN_TAKEOVER_TEMPLATES_PATH" -l "$subs_file" -c "$threads"
   fi
 }
 
-if [[ $# -ne 2 ]]; then
-    echo -e "\nUsage: $0 <subs.txt> <threads> <output_file> [optional argument: <output_file>]\n"
+# Ensure at least 2 arguments are provided
+if [[ $# -lt 2 || $# -gt 3 ]]; then
+    echo -e "\nUsage: $0 <subs.txt> <threads> [optional: <output_file>]\n"
     echo "Example:"
-    echo -e "  $0 subs.txt 5 subtake.out\n"
-    exit 0
+    echo -e "  $0 subs.txt 5        # Runs without output file"
+    echo -e "  $0 subs.txt 5 result.out  # Runs and saves output to result.out\n"
+    exit 1
 fi
+
+# Call function with provided arguments
+subtake "$@"
